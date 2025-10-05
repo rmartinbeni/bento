5f82c5d6402fbb37650e298fede4e4200ffcd216
Ricard Martin Benito <32068910+rmartinbeni@users.noreply.github.com>
2025-10-05 16:29:07 +0200

    fix(weather): Update weather API to use free service and adjust configuration (#4)

--- config.js
+++ config.js
@@ -27,13 +27,13 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '', // No se necesita clave de API para el nuevo proveedor
-	weatherIcons: 'Dark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'OneDark', // 'OneDark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '41.2342',
-	defaultLongitude: '2.1031',
+	defaultLatitude: '41.38879',
+	defaultLongitude: '2.15899',
 
 	// Autochange
 	autoChangeTheme: true,
